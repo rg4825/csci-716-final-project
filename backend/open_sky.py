72de3372d23ebd6bbbcaab95aa3150a49e7890fa
Ricky Gupta <rg4825@rit.edu>
2025-10-14 22:19:15 -0400

    switched to pyopensky

--- backend/open_sky.py
+++ backend/open_sky.py
@@ -1,10 +1,11 @@
 # file:         open_sky.py
 # description:  contains the functions for interacting with the OpenSky API
 
-from opensky_api import OpenSkyApi
+from pyopensky.rest import REST
 
 def test_api():
-    api = OpenSkyApi()
-    states = api.get_states()
-    for s in states.states:
-        print(s)
+    api = REST()
+    data = api.states()
+    for flight in data.itertuples():
+        if flight.origin_country == "Norway":
+            print(flight)
