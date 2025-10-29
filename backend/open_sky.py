a7f11acc30de8629b9639b48d262678166e04fcc
Ricky Gupta <rg4825@rit.edu>
2025-10-29 16:15:47 -0400

    more stub

--- backend/open_sky.py
+++ backend/open_sky.py
@@ -12,20 +12,28 @@ AIRPORTS_DF = pd.read_csv("../common/data/airports.csv")
 
 
 def get_flights(airport=DEFAULT_AIRPORT, radius=50):
+    """
+    TODO finish this
+    :param airport:
+    :param radius:
+    :return:
+    """
     row = AIRPORTS_DF.loc[AIRPORTS_DF["name"] == airport]
     lat, lng = row.iloc[0]["latitude_deg"], row.iloc[0]["longitude_deg"]
-    bbox = get_bbox(lat, lng, radius)
+    bbox = _get_bbox(lat, lng, radius)
 
     api = REST()
     data = api.states(bounds=tuple(bbox))
     return data
 
 
-def get_flight_trajectories():
-    pass
+def get_flight_trajectories(flights):
+    trajectories = []
+    for flight in flights:
+        pass
 
 
-def get_bbox(lat, lng, miles):
+def _get_bbox(lat, lng, miles):
     # sw, ne
     bearings = [225, 45]
     origin = Point(lat, lng)
