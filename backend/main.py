aa2cc1330fabac419eaebef7e2c3e076c1d24c3b
Ricky Gupta <rg4825@rit.edu>
2025-11-17 15:27:43 -0500

    more stubs in flight_ga

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,10 @@ import numpy as np
 
 from open_sky import get_flights_airport, get_flight_trajectories
 from genetic_algorithm import Organism, Population
+from voronoi import compute_voronoi_tree
 
-MAX_32 = 2 ** 32 - 1
+MAX_32 = 2**32 - 1
+
 
 def test_ga():
     import string
@@ -92,32 +94,46 @@ def test_open_sky():
             print(i)
         print()
 
+
 def flight_ga():
     airport = "Dallas Fort Worth International Airport"
     radius = 50
     cells = 20
 
     flights, bbox = get_flights_airport(airport, radius)
+    trajectories = get_flight_trajectories(flights)
+
     y_min, x_min, y_max, x_max = bbox[0], bbox[1], bbox[2], bbox[3]
     x_diff = x_max - x_min
     y_diff = y_max - y_min
 
     def _encode_chromosome(x, y):
-        norm_x, norm_y = (x - x_min) / x_diff, (y - y_min) / y_diff  # normalizes to [0, 1]
-        enc_x, enc_y = np.round(np.multiply(norm_x, MAX_32)).astype(np.uint32), np.round(np.multiply(norm_y, MAX_32)).astype(np.uint32)  # uniformly maps to uint32, a small amount of precision loss
-        x_bin, y_bin = np.binary_repr(enc_x, width=32), np.binary_repr(enc_y, width=32)  # 32 bit binary representation of uint32
+        norm_x, norm_y = (x - x_min) / x_diff, (
+            y - y_min
+        ) / y_diff  # normalizes to [0, 1]
+        enc_x, enc_y = np.round(np.multiply(norm_x, MAX_32)).astype(
+            np.uint32
+        ), np.round(np.multiply(norm_y, MAX_32)).astype(
+            np.uint32
+        )  # uniformly maps to uint32, a small amount of precision loss
+        x_bin, y_bin = np.binary_repr(enc_x, width=32), np.binary_repr(
+            enc_y, width=32
+        )  # 32 bit binary representation of uint32
         return x_bin, y_bin
 
     def _decode_chromosome(chromosome):
-        enc_x, enc_y = np.uint32(int(chromosome[0], 2)), np.uint32(int(chromosome[1], 2))
+        enc_x, enc_y = np.uint32(int(chromosome[0], 2)), np.uint32(
+            int(chromosome[1], 2)
+        )
         norm_x, norm_y = np.divide(enc_x, MAX_32), np.divide(enc_y, MAX_32)
         x, y = x_min + norm_x * x_diff, y_min + norm_y * y_diff
         return float(x), float(y)
 
-    def _decode_chromosomes_to_list(chromosomes):
-        return [_decode_chromosome(c) for c in chromosomes]
-
     def fitness_func(chromosomes):
+        seeds = [_decode_chromosome(c) for c in chromosomes]
+        seed_tree, voronoi_edges = compute_voronoi_tree(
+            seeds, x_min, y_min, x_max, y_max
+        )
         return 0.0
 
     def to_string(organism):
@@ -161,15 +177,24 @@ def flight_ga():
 
         return child_chromosome
 
-
     def reproduce_func(o1, o2, mutation=0.20):
-        child_chromosomes = np.array([_crossover_mutate_chromosomes(chrom1, chrom2, mutation) for chrom1, chrom2 in zip(o1.chromosomes, o2.chromosomes)], dtype=np.dtypes.StringDType)
-        child_chromosomes = np.array([_decode_chromosome(row) for row in child_chromosomes])
-        child_chromosomes = child_chromosomes[child_chromosomes[:,0].argsort()]
-        child_chromosomes = np.array([_encode_chromosome(row[0], row[1]) for row in child_chromosomes], dtype=np.dtypes.StringDType)
+        child_chromosomes = np.array(
+            [
+                _crossover_mutate_chromosomes(chrom1, chrom2, mutation)
+                for chrom1, chrom2 in zip(o1.chromosomes, o2.chromosomes)
+            ],
+            dtype=np.dtypes.StringDType,
+        )
+        child_chromosomes = np.array(
+            [_decode_chromosome(row) for row in child_chromosomes]
+        )
+        child_chromosomes = child_chromosomes[child_chromosomes[:, 0].argsort()]
+        child_chromosomes = np.array(
+            [_encode_chromosome(row[0], row[1]) for row in child_chromosomes],
+            dtype=np.dtypes.StringDType,
+        )
 
         return Organism(child_chromosomes, o1.fitness_func, to_string=o1.to_string)
-
 
     def create_random_organism():
         chromosomes = np.zeros((cells, 2))
@@ -179,8 +204,11 @@ def flight_ga():
             x, y = rng.uniform(x_min, x_max), rng.uniform(y_min, y_max)
             chromosomes[i] = x, y
 
-        chromosomes = chromosomes[chromosomes[:,0].argsort()]
-        chromosomes = np.array([_encode_chromosome(row[0], row[1]) for row in chromosomes], dtype=np.dtypes.StringDType)
+        chromosomes = chromosomes[chromosomes[:, 0].argsort()]
+        chromosomes = np.array(
+            [_encode_chromosome(row[0], row[1]) for row in chromosomes],
+            dtype=np.dtypes.StringDType,
+        )
 
         return Organism(
             chromosomes,
@@ -188,14 +216,15 @@ def flight_ga():
             to_string=to_string,
         )
 
-    o1 = create_random_organism()
-    print(f"o1:{o1}")
-
-    o2 = create_random_organism()
-    print(f"o2:{o2}")
-
-    child = reproduce_func(o1, o2)
-    print(f"child:{child}")
+    population = Population(
+        32,
+        fitness_func,
+        reproduce_func,
+        create_random_organism,
+        organism_to_string=to_string,
+    )
+    fittest = population.fully_evolve_population()
+    print(f"{fittest}")
 
 
 def main():
